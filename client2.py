95825b3ad3a11a074a0980a2bc3f668501f968ba
Halil İbrahim GÜNBULAK <67236125+Halil-ibrahim-GUNBULAK@users.noreply.github.com>
2021-05-22 16:36:32 +0300

    ChangeGui

---
+++ client2.py
@@ -0,0 +1,133 @@
+from PyQt5 import QtCore, QtWidgets
+import client_ui
+import connect_ui
+
+import sys
+import socket
+import random
+
+
+class ReceiveThread(QtCore.QThread):
+    signal = QtCore.pyqtSignal(str)
+
+    def __init__(self, client_socket):
+        super(ReceiveThread, self).__init__()
+        self.client_socket = client_socket
+
+    def run(self):
+        while True:
+            self.receive_message()
+
+    def receive_message(self):
+        message = self.client_socket.recv(1024)
+        message = message.decode()
+
+        print(message)
+        self.signal.emit(message)
+
+
+class Client(object):
+    def __init__(self):
+        self.messages = []
+        self.mainWindow = QtWidgets.QMainWindow()
+
+        # add widgets to the application window
+        self.connectWidget = QtWidgets.QWidget(self.mainWindow)
+        self.chatWidget = QtWidgets.QWidget(self.mainWindow)
+
+
+
+
+        self.connect_ui = connect_ui.Ui_Form()
+        self.connect_ui.setupUi(self.connectWidget)
+        self.connect_ui.pushButton.clicked.connect(self.btn_connect_clicked)
+
+        self.chatWidget.setHidden(True)
+        self.chat_ui = client_ui.Ui_Form()
+        self.chat_ui.setupUi(self.chatWidget)
+        self.chat_ui.pushButton.clicked.connect(self.send_message)
+
+        self.mainWindow.setGeometry(QtCore.QRect(1080, 20, 350, 500))
+        self.mainWindow.show()
+
+        self.tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def btn_connect_clicked(self):
+        host = self.connect_ui.hostTextEdit.toPlainText()
+        port = self.connect_ui.portTextEdit.toPlainText()
+        nickname = self.connect_ui.nameTextEdit.toPlainText()
+
+        if len(host) == 0:
+            host = "192.168.2.86"
+
+        if len(port) == 0:
+            port = 5555
+        else:
+            try:
+                port = int(port)
+            except Exception as e:
+                error = "Invalid port number \n'{}'".format(str(e))
+                print("[INFO]", error)
+                self.show_error("Port Number Error", error)
+
+        if len(nickname) < 1:
+            nickname = socket.gethostname()
+
+        nickname = nickname + "_" + str(random.randint(1, port))
+
+        if self.connect(host, port, nickname):
+            self.connectWidget.setHidden(True)
+            self.chatWidget.setVisible(True)
+
+            self.recv_thread = ReceiveThread(self.tcp_client)
+            self.recv_thread.signal.connect(self.show_message)
+            self.recv_thread.start()
+            print("[INFO] recv thread started")
+
+    def show_message(self, message):
+        self.chat_ui.textBrowser.append(message)
+
+    def connect(self, host, port, nickname):
+
+        try:
+            self.tcp_client.connect((host, port))
+            self.tcp_client.send(nickname.encode())
+
+            print("[INFO] Connected to server")
+
+            return True
+        except Exception as e:
+            error = "Unable to connect to server \n'{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Connection Error", error)
+            self.connect_ui.hostTextEdit.clear()
+            self.connect_ui.portTextEdit.clear()
+
+            return False
+
+    def send_message(self):
+        message = self.chat_ui.textEdit.toPlainText()
+        self.chat_ui.textBrowser.append("Me: " + message)
+
+        print("sent: " + message)
+
+        try:
+            self.tcp_client.send(message.encode())
+        except Exception as e:
+            error = "Unable to send message '{}'".format(str(e))
+            print("[INFO]", error)
+            self.show_error("Server Error", error)
+        self.chat_ui.textEdit.clear()
+
+    def show_error(self, error_type, message):
+        errorDialog = QtWidgets.QMessageBox()
+        errorDialog.setText(message)
+        errorDialog.setWindowTitle(error_type)
+        errorDialog.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        errorDialog.exec_()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    c = Client()
+    sys.exit(app.exec())
